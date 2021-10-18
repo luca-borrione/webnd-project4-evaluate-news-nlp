c31a11fc375875b239b07cf9e3b52dd3d681c62b
Luca Borrione <luca@email.com>
2021-10-18 23:50:14 +0100

    feat: importing the starter code from udacity

---
+++ src/client/js/formHandler.js
@@ -0,0 +1,18 @@
+import { checkForName } from './nameChecker';
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  // check what text was put into the form field
+  const formText = document.getElementById('name').value;
+  checkForName(formText);
+
+  console.log('::: Form Submitted :::');
+  fetch('http://localhost:8080/test')
+    .then((res) => res.json())
+    .then((res) => {
+      document.getElementById('results').innerHTML = res.message;
+    });
+}
+
+export { handleSubmit };
---
+++ src/server/index.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const express = require('express');
+const mockAPIResponse = require('./mockAPI.js');
+
+const app = express();
+
+app.use(express.static('dist'));
+
+console.log(__dirname);
+
+app.get('/', (req, res) => {
+  // res.sendFile('dist/index.html')
+  res.sendFile(path.resolve('src/client/views/index.html'));
+});
+
+// designates what port the app will listen to for incoming requests
+app.listen(8080, () => {
+  console.log('Example app listening on port 8080!');
+});
+
+app.get('/test', (req, res) => {
+  res.send(mockAPIResponse);
+});
