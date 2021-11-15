92df2ce4ed6a04135c4ace7d6954e41386feb7f1
Luca Borrione <luca@email.com>
2021-11-15 22:29:05 +0000

    chore: fix node modules dependencies

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
 
@@ -34,7 +34,7 @@ module.exports = {
   },
   optimization: {
     minimize: true,
-    minimizer: [new TerserPlugin(), new OptimizeCSSAssetsPlugin()],
+    minimizer: ['...', new TerserPlugin(), new CssMinimizerPlugin()],
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
